733a27a708dc5d2affc9fef90d3eae79d1bec618
Abhinav Dixit <abhinavdixit@Abhinavs-MacBook-Air.local>
2024-12-03 22:30:08 +0530

    login route protected and added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Body from './components/Body';
 import Login from './components/Login';
 import Profile from './components/Profile';
@@ -8,26 +8,39 @@ import appStore from './utils/appStore';
 import Feed from './components/Feed';
 import Connections from './components/Connections';
 import Requests from './components/Requests';
+import { useSelector } from 'react-redux';
+
+const ProtectedLoginRoute = ({ children }) => {
+  const user = useSelector((store) => store.user);
+  return user ? <Navigate to="/" replace /> : children;
+};
 
 function App() {
   return (
     <>
-    <Provider store={appStore}>
-      <BrowserRouter basename='/'>
-        <Routes>
-        <Route path='/' element={<Body/>}>
-         <Route path='/' element={<Feed/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/connections' element={<Connections/>}/>
-          <Route path='/requests' element={<Requests/>}/>
-          <Route path="*" element={<NotFound/>} />
-        </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route
+                path="/login"
+                element={
+                  <ProtectedLoginRoute>
+                    <Login />
+                  </ProtectedLoginRoute>
+                }
+              />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,95 +2,123 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL } from "../utils/constants";
 import { removeUser } from "../utils/userSlice";
-import Cookies from 'js-cookie';
 import { Link, useNavigate } from "react-router-dom";
-
+import logo from "../assets/logo.webp" 
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const user = useSelector(store => store.user);
-    const handleLogout = async ()=>{
-        axios.defaults.withCredentials = true;
+  const handleLogout = async () => {
+    axios.defaults.withCredentials = true;
 
-        try{
-            const userLogout = await axios.post(BASE_URL+"/logout", {}, {withCredentials:true});
-            dispatch(removeUser(userLogout.data));
-           // Cookies.remove('token');
-            navigate("/login");
-        }
-        catch(err){
-            console.log(err);
-        }
+    try {
+      const userLogout = await axios.post(
+        BASE_URL + "/logout",
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUser(userLogout.data));
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
     }
-    return(
-            <div className="navbar bg-base-100 z-10 fixed top-0 left-0">
-            <div className="navbar-start">
-                <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16M4 18h7" />
-                    </svg>
-                </div>
-                <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/requests">Requests</Link></li>
-                    <li><Link to="/connections">Connections</Link></li>
-                </ul>
-                </div>
+  };
+
+  return (
+    <div className="navbar bg-base-100 z-10 fixed top-0 left-0 shadow-md">
+      <div className="navbar-start">
+        {user && (
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
             </div>
-            <div className="navbar-center">
-                <a className="btn btn-ghost text-3xl italic font-extralight leading-loose ">bookmyheart</a>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/requests">Requests</Link>
+              </li>
+              <li>
+                <Link to="/connections">Connections</Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+      <div className="navbar-center">
+
+        <Link to={user&&"/"}  className="flex items-center gap-2">
+          <img src={logo} alt="BookMyHeart Logo" className="w-10 h-10" />
+          <span className="text-3xl italic font-extralight leading-loose">
+            BookMyHeart
+          </span>
+        </Link>
+      </div>
+      <div className="navbar-end flex items-center gap-4">
+        <button className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+        {user && (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar online"
+            >
+              <div className="w-10 rounded-full">
+                <img src={user.photoUrl} alt="User Avatar" />
+              </div>
             </div>
-            <div className="navbar-end">
-                <button className="btn btn-ghost btn-circle">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <button type="button" onClick={handleLogout}>
+                  Logout
                 </button>
-                {user &&(
-                <button className="btn btn-ghost btn-circle dropdown">
-                <div  tabIndex={1} role="button" className="indicator ">
-                <div className="avatar online">
-                    <div className="w-10 rounded-full">
-                        <img src={user.photoUrl} />
-                        <ul
-                    tabIndex={1}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><button type="button" onClick={handleLogout} href="">Logout</button></li>
-                </ul>
-                    </div>
-                 
-                    </div>
-                   
-                </div>
-                </button>
-                )}
-            </div>
-            </div>
-    )
-}
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
